5dfcbab06d23343571c6cf3059adb960da43d542
lililevin <levin.lili.rubin@gmail.com>
2019-12-14 18:07:21 -0500

    New

---
+++ TestNew/src/test/java/org/CAA/AppiumTest.java
@@ -0,0 +1,17 @@
+package org.CAA;
+
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppAndroid()
+    {
+        System.out.println("NativeAppAndroid");
+    }
+    @Test
+    public void NativeAppIOS()
+    {
+        System.out.println("NativeAppIOS");
+    }
+}
---
+++ TestNew/src/test/java/org/CAA/RestAPITest.java
@@ -0,0 +1,18 @@
+package org.CAA;
+
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+    @Test
+    public void PostJira()
+    {
+        System.out.println("PostJira");
+    }
+    @Test
+    public void deleteTwitter()
+    {
+        System.out.println("deleteTwitter");
+    }
+}
+
